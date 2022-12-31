949a9d4ba16cc885c3c51ed15ca124d23f6007a6
Nikhil Ranjan <96624909+then00bprogrammer@users.noreply.github.com>
2022-12-31 19:10:33 +0530

    Update kmp.cpp

--- kmp.cpp
+++ kmp.cpp
@@ -4,28 +4,9 @@ using namespace std;
 class SubstringSearch {
 public:
   /**
-   * Slow method of pattern matching
-   */
-  bool hasSubstring(string text, string pattern) {
-    int i = 0;
-    int j = 0;
-    int k = 0;
-    while (i < text.size() && j < pattern.size()) {
-      if (text[i] == pattern[j]) {
-        ++i;
-        ++j;
-      } else {
-        j = 0;
-        ++k;
-        i = k;
-      }
-    }
-    return (j == pattern.size());
-  }
-
-  /**
    * Compute temporary array to maintain size of suffix which is same as prefix
    * Time/space complexity is O(size of pattern)
+   * `https://medium.com/@aakashjsr/preprocessing-algorithm-for-kmp-search-lps-array-algorithm-50e35b5bb3cb` for LPS explanation and obviously Tushar Roy
    */
   vector<int> computeTemporaryArray(string pattern) {
     vector<int> lps(pattern.size());
